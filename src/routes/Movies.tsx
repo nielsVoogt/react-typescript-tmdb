1c56b82e05f3fc64c1a3540dddf24eb041e760ea
nielsVoogt <niels@branchonline.nl>
2019-04-29 18:36:49 +0200

    Getting my first response from tmdb

--- src/routes/Movies.tsx
+++ src/routes/Movies.tsx
@@ -1,10 +1,37 @@
 import React, { useEffect } from 'react'
 import { RouteComponentProps } from '@reach/router'
 import MovieCard from '../components/MovieCard'
-import axios from 'axios'
+import axios, { AxiosRequestConfig, AxiosPromise } from 'axios'
+
+type Movie = {
+  title: string
+  vote_average: number
+  // year: number
+  // id: number
+}
 
 const Movies: React.FunctionComponent<RouteComponentProps> = () => {
-  const apiKey = process.env.REACT_APP_TMDB_API_KEY
+  const url: string = `https://api.themoviedb.org/3/discover/movie?api_key=${
+    process.env.REACT_APP_TMDB_API_KEY
+  }&language=en-US&region=NL&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+  let movies
+
+  async function getMovies(): Promise<any> {
+    try {
+      const response = await axios.get(url)
+      movies = response.data.results.map((movie: Movie) => {
+        let { title, vote_average } = movie
+        return { title, vote_average }
+      })
+      console.log(movies)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getMovies()
+  })
 
   return (
     <div>
