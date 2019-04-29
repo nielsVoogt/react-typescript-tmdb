6c37eb476d3dc83cac61aafc23ffd33adbe9b443
nielsVoogt <niels@branchonline.nl>
2019-04-29 17:54:02 +0200

    Adding the tmdb logo

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import { Link } from '@reach/router'
+import { ReactComponent as Logo } from '../assets/logo.svg'
 
 const Navigation: React.FunctionComponent = () => {
   return (
     <div>
       <nav>
+        <Logo />
         <Link to="/">Movies</Link>
         <Link to="dashboard">Series</Link>
       </nav>
